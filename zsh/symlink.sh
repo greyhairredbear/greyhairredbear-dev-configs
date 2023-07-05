5cfa312e4972ecb6f3676e5035c498e469b73842
Peter Laggner <peter.laggner@gmx.at>
2023-07-06 00:59:49 +0200

    Update symlink.sh
    change order

--- zsh/symlink.sh
+++ zsh/symlink.sh
@@ -1,5 +1,5 @@
 export REPO_HOME=$HOME/dev/tool/greyhairredbear-dev-configs
-ln -s $REPO_HOME/zsh/.zshrc $HOME/.zshrc
-ln -s $REPO_HOME/zsh/.p10k.sh $HOME/.p10k.sh
 ln -s $REPO_HOME/zsh/.aliases $HOME/.aliases
 ln -s $REPO_HOME/zsh/.functions $HOME/.functions
+ln -s $REPO_HOME/zsh/.zshrc $HOME/.zshrc
+ln -s $REPO_HOME/zsh/.p10k.sh $HOME/.p10k.sh
