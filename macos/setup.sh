10e1a14b5aa6218318966add8075bb2878f203dc
Peter Laggner <peter.laggner@gmx.at>
2023-07-06 01:02:20 +0200

    install ohmyzsh

--- macos/setup.sh
+++ macos/setup.sh
@@ -1,2 +1,5 @@
 # install homebrew (see https://brew.sh)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
