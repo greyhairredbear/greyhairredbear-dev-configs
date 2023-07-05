b6a4f7d48c4decbab579e5eae78c2dd504c15e37
Peter Laggner <peter.laggner@gmx.at>
2023-07-06 01:40:38 +0200

    automate homebrew installation

--- macos/setup.sh
+++ macos/setup.sh
@@ -1,5 +1,7 @@
 # install homebrew (see https://brew.sh)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/peterlaggner/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
